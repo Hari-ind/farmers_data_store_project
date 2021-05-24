42db6883dc5fe88637847428ed41176e35117d25
hari <hari720k@gmail.com>
2021-05-24 18:13:01 +0530

    first commit

---
+++ precache-manifest.71b9113f880754d42196172a8b287f6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4049260a3491477abbfcd00579f8a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1673e32c4863eb4b878c",
+    "url": "/static/css/main.30b74978.chunk.css"
+  },
+  {
+    "revision": "991c9a2a9c90248a0a70",
+    "url": "/static/js/2.e0a49d96.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.e0a49d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1673e32c4863eb4b878c",
+    "url": "/static/js/main.4766bfcb.chunk.js"
+  },
+  {
+    "revision": "5823efb02d026ec3609d",
+    "url": "/static/js/runtime-main.1014f6ff.js"
+  }
+]);
